2d6097c368b639d95fc55f578f3554132744b71f
esc4n0rx <paulo.cunha@hortifruti.com.br>
2025-02-24 17:37:58 -0300

    Fixes command

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 import eventlet
-eventlet.monkey_patch()  
+eventlet.monkey_patch()  # Deve ser chamado antes de qualquer importação
 
 import os
 from flask import Flask
 from flask_cors import CORS
-from app import create_app, socketio
+from flask_socketio import SocketIO
 
+socketio = SocketIO(cors_allowed_origins="*")
+
+def create_app():
+    app = Flask(__name__)
+
+    allowed_origins = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+    CORS(app, resources={r"/*": {"origins": allowed_origins}})
+
+    app.config["SECRET_KEY"] = "your_secret_key"
+
+    socketio.init_app(app, cors_allowed_origins="*")
+
+    return app
 
 app = create_app()
 
-
-allowed_origins = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
-CORS(app, resources={r"/*": {"origins": allowed_origins}})
-
-
-with app.app_context():
-    if __name__ == '__main__':
-        port = int(os.environ.get("PORT", 5000))
-        socketio.run(app, host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
 
 
