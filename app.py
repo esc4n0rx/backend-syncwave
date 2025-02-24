5fce66d75b42aecf061645d268cd8160c638c2af
esc4n0rx <paulo.cunha@hortifruti.com.br>
2025-02-24 17:27:20 -0300

    fixes

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from app import create_app, socketio
-from flask_cors import CORS
+import eventlet
+eventlet.monkey_patch()  
+
 import os
+from flask import Flask
+from flask_cors import CORS
+from app import create_app, socketio
+
 
 app = create_app()
+
 
 allowed_origins = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 CORS(app, resources={r"/*": {"origins": allowed_origins}})
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+with app.app_context():
+    if __name__ == '__main__':
+        port = int(os.environ.get("PORT", 5000))
+        socketio.run(app, host='0.0.0.0', port=port)
+
 
