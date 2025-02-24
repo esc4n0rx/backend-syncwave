dc9aa32516126153238044b32087552ff2ebdf48
esc4n0rx <paulo.cunha@hortifruti.com.br>
2025-02-24 17:23:38 -0300

    Correção cors

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ from app import create_app, socketio
 from flask_cors import CORS
 import os
 
-
 app = create_app()
 
-origins = [
-    "https://syncwave-x.vercel.app/",
-    "http://localhost:3000",
-]
+allowed_origins = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+CORS(app, resources={r"/*": {"origins": allowed_origins}})
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host='0.0.0.0', port=port)
+
