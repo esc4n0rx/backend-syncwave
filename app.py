1c51b6ac259f137423e5780d230439f2d9d074d3
esc4n0rx <paulo.cunha@hortifruti.com.br>
2025-02-24 17:08:54 -0300

    Fixes

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from app import create_app, socketio
 from flask_cors import CORS
+import os
 
 
 app = create_app()
 
-CORS(app, origins=["http://localhost:3000"])
+origins = [
+    "https://syncwave.vercel.app/",
+    "http://localhost:3000",
+]
 
 if __name__ == '__main__':
-    socketio.run(app,host='0.0.0.0',port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
